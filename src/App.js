7345644dc05184effa8124d3bfa09d0eaa77d11d
ansuman5555 <ansumanghosh@infrrd.ai>
2020-01-03 16:35:33 +0530

    Setup pdfjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Pdf from "./PDF";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <div className="col-md-auto text-center">
+          <h1 className="text-white bg-info rounded">Fetch PDF by URL</h1>
+          <div className="border rounded">
+            <Pdf />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
